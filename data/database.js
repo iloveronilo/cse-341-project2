11a4b13fbeefc0ab15224acd01b336e6031c6d26
Ronilo Gulay <iloveronilo@gmail.com>
2025-02-10 11:42:53 +0800

    commit the get method

---
+++ data/database.js
@@ -0,0 +1,36 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const MongoClient = require('mongodb').MongoClient;
+
+let database;
+
+const initDB = (callback) => {
+    if (database) {
+        console.log('Database is already connected');
+        return callback(null, database);
+    }
+
+    MongoClient.connect(process.env.MONGODB_URL)
+        .then((client) => {
+            database = client;;
+            console.log('Database connected');
+            return callback(null, database);
+        })
+        .catch((err) => {
+            console.log('Error connecting to database');
+            return callback(err);
+        });   
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw new Error('Database is not connected');
+    }
+    return database;
+};
+
+module.exports = {
+    initDB,
+    getDatabase     
+};
